5473cdc8f1cb1eb47d1dc2dd852e44153275dca9
Luca Christen <lucachristen94@gmail.com>
2020-03-28 14:19:37 +0100

    Add drawer

--- lib/map.dart
+++ lib/map.dart
@@ -9,6 +9,7 @@ class MapView extends StatefulWidget {
 
 class MapViewState extends State<MapView> {
   Completer<GoogleMapController> _controller = Completer();
+  GlobalKey<ScaffoldState >scaffoldKey = GlobalKey<ScaffoldState>();
 
   static final CameraPosition _kZurich = CameraPosition(
     target: LatLng(47.36667, 8.54),
@@ -18,23 +19,59 @@ class MapViewState extends State<MapView> {
   @override
   Widget build(BuildContext context) {
     return new Scaffold(
-        appBar: AppBar(
-          title: Text('Overview'),
-        ),
-        body: GoogleMap(
-          mapType: MapType.hybrid,
-          initialCameraPosition: _kZurich,
-          onMapCreated: (GoogleMapController controller) {
-            _controller.complete(controller);
-          },
-        ),
-        floatingActionButton: FloatingActionButton.extended(
-          onPressed: () {
-            // Navigate to the second screen using a named route.
-            Navigator.pushNamed(context, '/second');
-          },
-          label: Text('DetailPage!'),
-          icon: Icon(Icons.directions_boat),
-        ));
+      key: scaffoldKey,
+      drawer : buildDrawer(context),
+      body: Stack(
+        children: <Widget> [
+          new Center(
+            child : GoogleMap(
+              mapType: MapType.hybrid,
+              initialCameraPosition: _kZurich,
+              onMapCreated: (GoogleMapController controller) {
+                _controller.complete(controller);
+              }
+            )
+          ),
+          Positioned(
+            left: 10,
+            top: 20,
+            child: IconButton(
+              icon: Icon(Icons.menu, color: Colors.white),
+              onPressed: () => scaffoldKey.currentState.openDrawer(),
+            ),
+          )
+        ]
+      ),
+      floatingActionButton: FloatingActionButton.extended(
+        onPressed: () {
+          // Navigate to the second screen using a named route.
+          Navigator.pushNamed(context, '/second');
+        },
+        label: Text('DetailPage!'),
+        icon: Icon(Icons.directions_boat),
+      ));
+  }
+
+  Drawer buildDrawer(BuildContext context) {
+    return new Drawer(
+        child: ListView(
+          // Remove any padding from the ListView.
+          padding: EdgeInsets.zero,
+          children: <Widget>[
+            DrawerHeader(
+              child: Text('Insert Logo Here'),
+              decoration: BoxDecoration(
+                color: Colors.blue,
+              ),
+            ),
+            ListTile(
+              title: Text('Offer a workplace'),
+              onTap: () {
+                // Do nothing for now
+                Navigator.pop(context);
+              },
+            ),
+          ]));
   }
 }
+
